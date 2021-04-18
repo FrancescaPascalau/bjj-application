7d57791672ddbd4b20973ee0edbd66f2ba97ba2e
pvaldesoiro <pvaldesoiro@gmail.com>
2021-04-18 19:38:18 +0200

    Fix eslint problems

--- src/plugins/firebase.js
+++ src/plugins/firebase.js
@@ -3,7 +3,7 @@ import firebase from '@firebase/app';
 import '@firebase/auth';
 import '@firebase/firestore';
 
-axios.get('/__/firebase/init.json').then(async response => {
+axios.get('/__/firebase/init.json').then(async (response) => {
   firebase.initializeApp(await response.data);
 });
 
